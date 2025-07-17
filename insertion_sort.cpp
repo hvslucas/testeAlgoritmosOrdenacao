587e1c66d15ccd5a46b93734d29b12fb187f079e
Lucas Henrique <123264352+hvslucas@users.noreply.github.com>
2025-07-16 23:04:06 -0300

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -11,16 +11,16 @@ const string filename = "instancias-num/num.100000.4.in"; // alterar
 int main() {
     
     ifstream inFile(filename);
-
+    
     vector<int> data;
     int num;
     while (inFile >> num) {
         data.push_back(num);
     }
     inFile.close();
-
+    
     auto start = chrono::high_resolution_clock::now();
-
+    
     for (int i = 1; i < data.size(); ++i) {
         int aux = data[i];
         int j = i - 1;
@@ -30,16 +30,16 @@ int main() {
         }
         data[j + 1] = aux;
     }
-
+    
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double> duration = end - start;
-
+    
     // Salvar
     ofstream outFile("resultados.txt", ios::app);
-
+    
     outFile << "Arquivo " << filename << ":\n";
     outFile << "insertionSort: " << duration.count() << " segundos\n";
     outFile.close();
-
+    
     return 0;
 }
